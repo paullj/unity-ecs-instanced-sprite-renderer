bd72571b1b0281b31f7c05a0a5fcc6b6d05dcc7e
Matěj Ošanec <matej.osanec@geewa.com>
2018-10-26 12:51:40 +0200

    Updated to entities version 0.0.12-preview.19

--- Assets/Scenes/Sprite Rendering Example/SpriteRendererMovementSystem.cs
+++ Assets/Scenes/Sprite Rendering Example/SpriteRendererMovementSystem.cs
@@ -1,31 +1,32 @@
-﻿using Unity.Entities;
-using Unity.Mathematics;
-using Unity.Transforms2D;
-using UnityEngine;
+﻿using Unity.Burst;
+using Unity.Entities;
 using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
 
 namespace toinfiniityandbeyond.Examples
 {
 	//Just gives the animals a little movement as an example
 	public class EntityMovementSystem : JobComponentSystem
 	{
-		[ComputeJobOptimization]
-		struct PositionJob : IJobProcessComponentData<Position2D>
+		[BurstCompile]
+		struct PositionJob : IJobProcessComponentData<Position>
 		{
 			public float dt;
 
-			public void Execute(ref Position2D position)
+			public void Execute(ref Position position)
 			{
-				float wobbleX = Mathf.PerlinNoise(position.Value.x, position.Value.y) - 0.5f;
-				float wobbleY = Mathf.PerlinNoise(position.Value.y, position.Value.x) - 0.5f;
-				position.Value += dt * new float2(wobbleX, wobbleY);
+				float wobbleX = Mathf.PerlinNoise(position.Value.x, position.Value.z) - 0.5f;
+				float wobbleY = Mathf.PerlinNoise(position.Value.z, position.Value.x) - 0.5f;
+				position.Value += dt * new float3(wobbleX, 0, wobbleY);
 			}
 		}
 
 		protected override JobHandle OnUpdate(JobHandle inputDeps)
 		{
 			var job = new PositionJob() {dt = Time.deltaTime};
-			return job.Schedule(this, 64, inputDeps);
+			return job.Schedule(this, inputDeps);
 		}
 	}
 }
